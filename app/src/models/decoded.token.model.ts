86633f0df5c36f2048163ea78f18572503f2de85
Ensar Kovankaya <ensar@kovankaya.com>
2018-08-07 20:01:49 +0300

    change token model

--- app/src/models/decoded.token.model.ts
+++
@@ -1,9 +0,0 @@
-import { Role } from './role.model';
-
-export interface IDecodedTokenModel {
-    id: string;
-    role: Role;
-    email: string;
-    iat: number;
-    exp: number;
-}
---
+++ app/src/models/token.model.ts
@@ -0,0 +1,40 @@
+import { Role } from './role.model';
+
+export interface IAccessTokenPayload {
+    sub: string;
+    aud: Role;
+    iat: number;
+    exp: number;
+    jti: string;
+    data?: any;
+}
+
+export interface IRefreshTokenPayload {
+    sub: string;
+    nbf: number;
+    iat: number;
+    exp: number;
+}
+
+export interface IDecodedTokenModel<T> {
+    header: {
+        alg: string;
+        typ: string;
+    };
+    payload: T;
+    signature: string;
+}
+
+export interface IAuthToken {
+    accessToken: {
+        tokenType: string;
+        token: string;
+        expiresAt: number;
+    };
+    refreshToken: {
+        tokenType: string;
+        token: string;
+        expiresAt: number;
+        notBefore: number;
+};
+}
