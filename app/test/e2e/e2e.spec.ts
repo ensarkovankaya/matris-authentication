ba9e2dbc6a7ece6c8b5ed448a9e281d2c5525a03
Ensar Kovankaya <ensar@kovankaya.com>
2018-08-09 22:40:47 +0300

    change Password tests remove Verify tests

--- app/test/e2e/e2e.spec.ts
+++ app/test/e2e/e2e.spec.ts
@@ -2,14 +2,13 @@ import { expect } from 'chai';
 import { createServer, Server as HttpServer } from 'http';
 import { after, before, describe, it } from 'mocha';
 import "reflect-metadata";
+import { IAuthToken } from '../../src/models/token.model';
 import { Server } from '../../src/server';
-import { TokenDataSource } from '../data/tokens/token';
 import { Database } from '../data/valid/database';
 import { IDBUserModel } from '../data/valid/db.model';
 import { HttpClient } from './http.client';
 
 const DATABASE = new Database();
-const TOKENS = new TokenDataSource();
 
 let server: HttpServer;
 
@@ -38,15 +37,27 @@ describe('E2E', () => {
             try {
                 const users = DATABASE.multiple(10, (d: IDBUserModel) => d.active === true && d.deleted === false);
                 for (const user of users) {
-                    const response = await client.password<{data: string}>({email: user.email, password: user.email});
+                    const response = await client.password<{
+                        data: IAuthToken
+                    }>({email: user.email, password: user.email, atExpiresIn: '1h', rtExpiresIn: '2h'});
                     expect(response.status).to.be.eq(200);
                     expect(response.data).to.be.an('object');
                     expect(response.data).to.have.key('data');
-                    expect(response.data.data).to.be.a('string');
-                    expect(response.data.data.length).to.be.gte(150);
+                    expect(response.data.data).to.be.a('object');
+                    expect(response.data.data).to.have.keys(['accessToken', 'refreshToken']);
+                    expect(response.data.data.accessToken).to.be.a('object');
+                    expect(response.data.data.refreshToken).to.be.a('object');
+                    expect(response.data.data.accessToken.token).to.be.a('string');
+                    expect(response.data.data.accessToken.tokenType).to.be.eq('Bearer');
+                    expect(response.data.data.accessToken.expiresAt).to.be.a('number');
+                    expect(response.data.data.refreshToken.token).to.be.a('string');
+                    expect(response.data.data.refreshToken.tokenType).to.be.eq('Bearer');
+                    expect(response.data.data.refreshToken.expiresAt).to.be.a('number');
+                    expect(response.data.data.refreshToken.notBefore).to.be.a('number');
                 }
             } catch (e) {
                 console.error(e);
+                console.log(e.data.errors);
                 throw e;
             }
         }).timeout(4000);
@@ -56,7 +67,10 @@ describe('E2E', () => {
                 .concat([{email: 'mail@mail.com', password: '12345678'}] as any);
             for (const user of users) {
                 try {
-                    await client.password<{data: string}>({email: user.email, password: user.email});
+                    await client.password<{
+                        data: null,
+                        errors: Array<{msg: string, location: string, param: string}>
+                    }>({email: user.email, password: user.email, atExpiresIn: '1h', rtExpiresIn: '2h'});
                     throw new ShouldNotSucceed();
                 } catch (e) {
                     expect(e.name).to.be.eq('HttpClientError');
@@ -74,7 +88,10 @@ describe('E2E', () => {
             const users = DATABASE.multiple(10, (d: IDBUserModel) => d.active === false && d.deleted === false);
             for (const user of users) {
                 try {
-                    await client.password<{data: string}>({email: user.email, password: user.email});
+                    await client.password<{
+                        data: null,
+                        errors: Array<{msg: string, location: string, param: string}>
+                    }>({email: user.email, password: user.email, atExpiresIn: '1h', rtExpiresIn: '2h'});
                     throw new ShouldNotSucceed();
                 } catch (e) {
                     expect(e.name).to.be.eq('HttpClientError');
@@ -92,7 +109,10 @@ describe('E2E', () => {
             const users = DATABASE.multiple(10, (d: IDBUserModel) => d.active === true && d.deleted === false);
             for (const user of users) {
                 try {
-                    await client.password<{data: string}>({email: user.email, password: '12345678'});
+                    await client.password<{
+                        data: null,
+                        errors: Array<{msg: string, location: string, param: string}>
+                    }>({email: user.email, password: '12345678', atExpiresIn: '1h', rtExpiresIn: '2h'});
                     throw new ShouldNotSucceed();
                 } catch (e) {
                     expect(e.name).to.be.eq('HttpClientError');
@@ -107,90 +127,6 @@ describe('E2E', () => {
                 }
             }
         }).timeout(4000);
-    });
-
-    describe('Verify', () => {
-
-        it('should return decoded tokens', async () => {
-            // Get valid tokens from source
-            const tokens = TOKENS.multiple(20, t => t.type === 'valid');
-            const secret = process.env.JWT_SECRET;
-
-            for (const data of tokens) {
-                if (secret !== data.secret) {
-                    throw new Error(`Secrets not matched. Expected: "${data.secret}" Actual: "${secret}"`);
-                }
-                const response = await client.verify<{data: {
-                    id: string;
-                    email: string;
-                    role: string;
-                    iat: number;
-                    exp: number
-                }}>({token: data.token});
-                expect(response.status).to.be.eq(200);
-                expect(response.data).to.be.an('object');
-                expect(response.data).to.have.key('data');
-                expect(response.data.data).to.be.an('object');
-                expect(response.data.data).to.have.keys(['id', 'email', 'role', 'iat', 'exp']);
-
-                expect(response.data.data.id).to.be.a('string');
-                expect(response.data.data.id).to.be.eq(data.id);
-
-                expect(response.data.data.email).to.be.a('string');
-                expect(response.data.data.email).to.be.eq(data.email);
-
-                expect(response.data.data.role).to.be.a('string');
-                expect(response.data.data.role).to.be.eq(data.role);
-
-                expect(response.data.data.iat).to.be.a('number');
-                expect(response.data.data.iat).to.be.eq(data.iat);
-
-                expect(response.data.data.exp).to.be.a('number');
-                expect(response.data.data.exp).to.be.eq(data.exp);
-            }
-        });
-
-        it('should response with token expired', async () => {
-            // Get valid tokens from source
-            const tokens = TOKENS.multiple(20, t => t.type === 'expired');
-            const secret = process.env.JWT_SECRET;
-
-            for (const data of tokens) {
-                try {
-                    if (secret !== data.secret) {
-                        throw new Error(`Secrets not matched. Expected: "${data.secret}" Actual: "${secret}"`);
-                    }
-                    await client.verify({token: data.token});
-                } catch (e) {
-                    expect(e.name).to.be.eq('HttpClientError');
-                    expect(e.status).to.be.eq(400);
-                    expect(e.data).to.be.an('object');
-                    expect(e.data).to.have.keys(['errors', 'data']);
-                    expect(e.data.data).to.be.eq(null);
-                    expect(e.data.errors).to.be.an('array');
-                    expect(e.data.errors).to.be.deep.eq([{ msg: 'TokenExpired', location: 'body', param: 'token' }]);
-                }
-            }
-        });
-
-        it('should response with invalid token', async () => {
-            // Get valid tokens from source
-            const tokens = TOKENS.multiple(20, t => t.type === 'invalid');
-
-            for (const data of tokens) {
-                try {
-                    await client.verify({token: data.token});
-                } catch (e) {
-                    expect(e.name).to.be.eq('HttpClientError');
-                    expect(e.status).to.be.eq(400);
-                    expect(e.data).to.be.an('object');
-                    expect(e.data).to.have.keys(['errors', 'data']);
-                    expect(e.data.data).to.be.eq(null);
-                    expect(e.data.errors).to.be.an('array');
-                    expect(e.data.errors).to.be.deep.eq([{ msg: 'InvalidToken', location: 'body', param: 'token' }]);
-                }
-            }
-        });
     });
 });
 
